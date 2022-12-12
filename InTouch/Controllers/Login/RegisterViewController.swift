fa402c77e3e268e585611298903484f54273810c
Kashchenko <cashenkoruslan@yandex.by>
2022-12-13 01:13:58 +0300

    fix bug with photo picker

--- InTouch/Controllers/Login/RegisterViewController.swift
+++ InTouch/Controllers/Login/RegisterViewController.swift
@@ -7,13 +7,9 @@ final class RegisterViewController: UIViewController, UINavigationControllerDele
     private let registerView = RegisterView()
     private var registerButton = UIButton()
     private var backButton = UIButton()
-//    private var firstNameTextField = UITextField()
-//    private var lastNameTextFeld = UITextField()
     private var emailTextField = UITextField()
     private var passwordTextField = UITextField()
     private var confirmPasswordTextField = UITextField()
-//    private var userPhotoView = UIImageView()
-    
     
     //MARK: - Lifecycles
     
@@ -27,13 +23,7 @@ final class RegisterViewController: UIViewController, UINavigationControllerDele
         setAppearance()
         setTargets()
         setDelegates()
-//        setTapRecognizer()
-        
         hideKeyboardWhenTappedAround()
-        
-        //Notification for keyboard
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     //MARK: - Registration
@@ -42,13 +32,9 @@ final class RegisterViewController: UIViewController, UINavigationControllerDele
         guard let email = emailTextField.text,
               let password = passwordTextField.text,
               let confirmPassword = confirmPasswordTextField.text,
-//              let firstName = firstNameTextField.text,
-//              let lastName = lastNameTextFeld.text,
               !email.isEmpty,
               !password.isEmpty,
               !confirmPassword.isEmpty
-              //!firstName.isEmpty,
-              //!lastName.isEmpty
         else {
             return showAlert(title: "Ошибка ввода",
                              message: "Проверьте всю введенную информацию.")
@@ -70,13 +56,7 @@ final class RegisterViewController: UIViewController, UINavigationControllerDele
         present(settingsProfileVC, animated: true)
     }
     
-    
-    
     //MARK: - Behaviour
-    
-    @objc private func userPhotoImagePressed() {
-        presentPhotoActionAlert()
-    }
     
     private func setTargets() {
         registerButton.addTarget(self,
@@ -85,13 +65,6 @@ final class RegisterViewController: UIViewController, UINavigationControllerDele
         backButton.addTarget(self,
                              action: #selector(backButtonPressed), for: .touchUpInside)
     }
-    
-//    private func setTapRecognizer() {
-//        let photoTapRecognizer = UITapGestureRecognizer(target: self,
-//                                                        action: #selector(userPhotoImagePressed))
-//        userPhotoView.isUserInteractionEnabled = true
-//        userPhotoView.addGestureRecognizer(photoTapRecognizer)
-//    }
     
     @objc private func backButtonPressed() {
         dismiss(animated: true)
@@ -105,26 +78,9 @@ final class RegisterViewController: UIViewController, UINavigationControllerDele
     
     //MARK: - Appearance
     
-//    @objc func keyboardWillShow(notification: NSNotification) {
-//        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-//            if self.view.frame.origin.y == 0 {
-//                self.view.frame.origin.y -= keyboardSize.height
-//            }
-//        }
-//    }
-//    
-//    @objc func keyboardWillHide(notification: NSNotification) {
-//        if self.view.frame.origin.y != 0 {
-//            self.view.frame.origin.y = 0
-//        }
-//    }
-    
     private func setAppearance() {
-//        userPhotoView = registerView.userPhotoView
         backButton = registerView.backButton
         registerButton = registerView.registerButton
-//        firstNameTextField = registerView.firstNameTextField
-//        lastNameTextFeld = registerView.lastNameTextField
         emailTextField = registerView.emailTextField
         passwordTextField = registerView.passwordTextField
         confirmPasswordTextField = registerView.confirmPasswordTextField
@@ -146,10 +102,6 @@ extension RegisterViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         switch textField {
-//        case firstNameTextField:
-//            lastNameTextFeld.becomeFirstResponder()
-//        case lastNameTextFeld:
-//            emailTextField.becomeFirstResponder()
         case emailTextField:
             passwordTextField.becomeFirstResponder()
         case passwordTextField:
@@ -160,61 +112,5 @@ extension RegisterViewController: UITextFieldDelegate {
             break
         }
         return true
-    }
-}
-
-//MARK: - UIImagePickerControllerDelegate
-
-extension RegisterViewController: UIImagePickerControllerDelegate {
-    
-    private func presentPhotoActionAlert() {
-        let changeAlert = UIAlertController(title: "Фото для профиля",
-                                            message: "Хотите создать новое фото или выбрать из существующих?",
-                                            preferredStyle: .actionSheet)
-        
-        changeAlert.addAction(UIAlertAction(title: "Сделать фото",
-                                            style: .default,
-                                            handler: { [weak self] _ in
-            self?.presentCamera()
-        }))
-        
-        changeAlert.addAction(UIAlertAction(title: "Выбрать из галереи",
-                                            style: .default,
-                                            handler: { [weak self] _ in
-            self?.presentPhotoPicker()
-        }))
-        
-        changeAlert.addAction(UIAlertAction(title: "Отменить",
-                                            style: .cancel))
-        
-        present(changeAlert, animated: true)
-    }
-    
-    private func presentPhotoPicker() {
-        let vc = UIImagePickerController()
-        vc.sourceType = .photoLibrary
-        vc.delegate = self
-        vc.allowsEditing = true
-        present(vc, animated: true)
-    }
-    
-    private func presentCamera() {
-        let vc = UIImagePickerController()
-        vc.sourceType = .camera
-        vc.delegate = self
-        vc.allowsEditing = true
-        present(vc, animated: true)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        picker.dismiss(animated: true)
-//        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
-//            return
-//        }
-//        userPhotoView.image = image
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true)
     }
 }
--- InTouch/Controllers/Login/SettingsProfileViewController.swift
+++ InTouch/Controllers/Login/SettingsProfileViewController.swift
@@ -1,11 +1,14 @@
 import UIKit
 
-class SettingsProfileViewController: UIViewController {
+class SettingsProfileViewController: UIViewController, UINavigationControllerDelegate {
     
     //MARK: - UI Constants
     
     private let settingsProfileView = SettingsProfileView()
     private var backButton = UIButton()
+    private var userPhotoView = UIImageView()
+    
+    //MARK: - Lifecycles
     
     override func loadView() {
         super.loadView()
@@ -15,7 +18,14 @@ class SettingsProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         backButton = settingsProfileView.backButton
+        hideKeyboardWhenTappedAround()
+        setUIValues()
+        setTapRecognizer()
         addTargets()
+        
+        //Notification for keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     //MARK: - Behavior
@@ -24,7 +34,94 @@ class SettingsProfileViewController: UIViewController {
         backButton.addTarget(self, action: #selector(backButtonPressed), for: .touchUpInside)
     }
     
+    @objc private func userPhotoImagePressed() {
+        presentPhotoActionAlert()
+    }
+    
+    private func setTapRecognizer() {
+        let photoTapRecognizer = UITapGestureRecognizer(target: self,
+                                                        action: #selector(userPhotoImagePressed))
+        userPhotoView.isUserInteractionEnabled = true
+        userPhotoView.addGestureRecognizer(photoTapRecognizer)
+    }
+    
     @objc private func backButtonPressed() {
         dismiss(animated: true)
+    }
+    
+    //MARK: - Appearance
+    
+    private func setUIValues() {
+        userPhotoView = settingsProfileView.userPhotoView
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+}
+
+//MARK: - UIImagePickerControllerDelegate
+
+extension SettingsProfileViewController: UIImagePickerControllerDelegate {
+    
+    private func presentPhotoActionAlert() {
+        let changeAlert = UIAlertController(title: "Фото для профиля",
+                                            message: "Хотите создать новое фото или выбрать из существующих?",
+                                            preferredStyle: .actionSheet)
+        
+        changeAlert.addAction(UIAlertAction(title: "Сделать фото",
+                                            style: .default,
+                                            handler: { [weak self] _ in
+            self?.presentCamera()
+        }))
+        
+        changeAlert.addAction(UIAlertAction(title: "Выбрать из галереи",
+                                            style: .default,
+                                            handler: { [weak self] _ in
+            self?.presentPhotoPicker()
+        }))
+        
+        changeAlert.addAction(UIAlertAction(title: "Отменить",
+                                            style: .cancel))
+        
+        present(changeAlert, animated: true)
+    }
+    
+    private func presentPhotoPicker() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    private func presentCamera() {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        userPhotoView.image = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
     }
 }
